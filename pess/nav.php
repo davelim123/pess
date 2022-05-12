6d4349fb711b4085071a4c03b5392a1d1216d917
davelim123 <105028236+davelim123@users.noreply.github.com>
2022-05-12 12:36:03 +0800

    Add files via upload

---
+++ pess/nav.php
@@ -0,0 +1,8 @@
+<h1>Police Emergency Service System</h1>
+<nav id="Headerstyle">
+		<a href="logcall.php">Log call</a>
+		<a href="Update.php">Update</a>
+		<a href="#">Report</a>
+		<a href="#">History</a>
+</nav>
+		
